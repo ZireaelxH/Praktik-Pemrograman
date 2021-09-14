7ec1b542d1437540c44fc5a8dac8de128f38e5b5
Hasto <hastorostamhadi@gmail.com>
2021-09-14 14:32:59 +0700

    menambahkan komentar berupa nama dan nim

--- Pertemuan 2/Hello.java
+++ Pertemuan 2/Hello.java
@@ -1,5 +1,10 @@
+/*
+NIM : 21537144023
+Nama : Hasto Rostamhadi
+*/
 public class Hello {
     public static void main(String[] args) {
+        // untuk menampilkan teks
         System.out.println("Hello world");
     }
 }
