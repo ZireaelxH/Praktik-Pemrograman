da8f190a7ff11be1bf53587a4d4330a7fb1f9e6a
Hasto <hastorostamhadi@gmail.com>
2021-09-14 17:43:26 +0700

    tugas pertemuan 3

---
+++ Pertemuan 3/Identitas.java
@@ -0,0 +1,18 @@
+public class Identitas {
+    public static void main(String[] args) {
+        String namaLengkap = "Hasto Rostamhadi";
+        String nim = "21537144023";
+        int angkatan = 2021;
+        String programStudi = "Teknologi Informasi";
+        String jurusan = "JPTEI";
+        String fakultas = "Teknik";
+        double ipkHarapan = 4;
+        System.out.println("Nama saya " + namaLengkap);
+        System.out.println("NIM saya " + nim);
+        System.out.println("Saya angkatan tahun " + angkatan);
+        System.out.println("Program Studi " + programStudi);
+        System.out.println("Jurusan di " + jurusan);
+        System.out.println("Fakultas " + fakultas);
+        System.out.println("IPK harapan saya adalah IPK " + ipkHarapan);
+    }
+}
---
+++ Pertemuan 3/Identitas.py
@@ -0,0 +1,14 @@
+namaLengkap = "Hasto Rostamhadi"
+nim = 21537144023
+angkatan = 2021
+programStudi = "Teknologi Informasi"
+jurusan = "JPTEI"
+fakultas = "Teknik"
+ipkHarapan = 4.0
+print("Nama lengkap saya adalah " + namaLengkap)
+print("NIM saya adalah " + str(nim))
+print("Saya angkatan tahun " + str(angkatan))
+print("Program Studi " + programStudi)
+print("Jurusan di " + jurusan)
+print("Fakultas " + fakultas)
+print(ipkHarapan)
