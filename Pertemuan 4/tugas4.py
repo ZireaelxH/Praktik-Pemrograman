af3befbb804521a36029c36641a107fde91dcdc1
Hasto <hastorostamhadi@gmail.com>
2021-09-25 23:47:00 +0700

    tugas pertemuan 4

---
+++ Pertemuan 4/tugas4.py
@@ -0,0 +1,19 @@
+print("Masukkan nilai a = ")
+a = input()
+print("Masukkan nilai b = ")
+b = input()
+
+hasilJumlah = int(a) + int(b)
+print("Hasil a + b = " + str(hasilJumlah))
+
+hasilKurang = int(a) - int(b)
+print("Hasil a - b = " + str(hasilKurang))
+
+hasilKali = int(a) * int(b)
+print("Hasil a * b = " + str(hasilKali))
+
+hasilBagi = int(a) / int(b)
+print("Hasil a / b = " + str(hasilBagi))
+
+hasilModulus = int(a) % int(b)
+print("Hasil a % b = " + str(hasilModulus))
