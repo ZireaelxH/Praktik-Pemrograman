caf5a9d2cd0a7224bb78db92d73a4253a50ccaec
Hasto <hastorostamhadi@gmail.com>
2021-10-17 21:18:48 +0700

    belajar for dan nested for

---
+++ Pertemuan 7/BelajarForPola.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BelajarForPola {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukan nilai n = ");
+        int n = scanner.nextInt();
+        for (int i = 1; i<=n; i++) {
+            for (int j = 1; j<=i; j++) {
+                if (i % 2 == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("#");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
